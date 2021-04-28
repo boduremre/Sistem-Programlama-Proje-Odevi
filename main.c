24a34dd574166ceb5ede5852c29a8e45629d1876
Emre Bodur <35879677+boduremre@users.noreply.github.com>
2021-04-28 11:23:27 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,11 @@
-// Fields ile ilgili örnek
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "jrb.h"
 #include "fields.h"
 
-int main()
+int main(int argc, char **argv)
 {
-  IS is;
-
-  // Yeni bir dosya okuyucusu tanımla
-  is = new_inputstruct(NULL);  // NULL --> stdin
-
-  get_line(is);
-
-  printf("Kelime sayısı: %d\n", is->NF);
-
-  if(is->NF > 2)
-    printf("İkinci kelime: %s\n", is->fields[1]);
-  else
-    printf("İkinci kelime yok\n");
-  
-  // Çıkmadan önce bellekte kullanılan yerlerin geri bırakılması
-  jettison_inputstruct(is);
+  printf("Merhaba Dünya!\n");
+  return 0;
 }
