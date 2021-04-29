99e3e8396742b78fa458512924c1ea31143ada58
Emre Bodur <35879677+boduremre@users.noreply.github.com>
2021-04-29 18:53:07 +0300

    main.c dosyası oluşturuldu.

---
+++ src/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fields.h"
+
+int main(int argc, char **argv)
+{
+  IS is;
+  int i;
+
+  /* Open the file as an inputstruct.  Error check. */
+  is = new_inputstruct(".kilit");
+  if (is == NULL)
+  {
+    perror(argv[1]);
+    exit(1);
+  }
+
+  /* Read each line with get_line().  Print out each word. */
+  while (get_line(is) >= 0)
+  {
+    for (i = 0; i < is->NF; i++)
+    {
+      printf("%d: %s\n", is->line, is->fields[i]);
+    }
+  }
+
+  jettison_inputstruct(is);
+  return 0;
+}
