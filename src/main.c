01666088e634e8ecdeb2b78bae2ce30e1310818b
ssenai18 <37250388+ssenai18@users.noreply.github.com>
2021-05-13 00:58:08 +0300

    main içerisinde dosyaların şifrelemesi ve çözülmesi için gerekli satırlar eklendi.

--- src/main.c
+++ src/main.c
@@ -17,5 +17,27 @@ int main(int argc, char **argv)
   
   // Kilit yapısı eklendi.
   Kilit *k;
+  
+  // field ile giriş metin dosyasına erişiliyor
+  IS is2;
+  is2 = new_inputstruct(argv[2]);
+  if (is2 == NULL)
+  {
+    perror(argv[1]);
+    exit(1);
+  }
+
+  if (!strcmp(argv[1], "-e"))
+  {
+    printf("%s =  encripted işlemi yapılıyor.\n", argv[1]);
+    // burada dosya şifrelenecek
+  }
+
+  if (!strcmp(argv[1], "-d"))
+  {
+    printf("%s = decripted işlemi yapılıyor.\n", argv[1]);
+    // burada giriş dosyasından şifre çözülecek
+  }
+  
   return 0;
 }
