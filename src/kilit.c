a6a173d8b2bc2cace9555a2a583becc41a0d0fa3
ssenai18 <37250388+ssenai18@users.noreply.github.com>
2021-05-14 16:05:45 +0300

    kilit dosyası okuma/hata satırları eklendi.

--- src/kilit.c
+++ src/kilit.c
@@ -9,3 +9,58 @@ void print_jrb();
 void fill_jrb_from_kilit_file(JRB kilit_encrypt, JRB kilit_decrypt);
 void encrypt_file(IS is2, JRB kilitler, char *hedef_dosya_adi);
 void decrypt_file(IS is, JRB kilitlerDec, char *hedef_dosya_adi);
+void fill_jrb_from_kilit_file(JRB kilit_encrypt, JRB kilit_decrypt)
+{
+    IS is;
+    Kilit *p;
+
+    //kilit dosyası okunuyor.
+    is = new_inputstruct(".kilit");
+    if (is == NULL)
+    {
+        perror("Huffman kilit dosyası bulunamadı!");
+        exit(1);
+    }
+
+    /* Read each line with get_line(). */
+    while (get_line(is) >= 0)
+    {
+
+        // ilk ve son satır değilse
+        // ilk satır daima { başlar ve son satır daima } ile biter.
+        if (strchr(is->fields[0], '{') == NULL && strchr(is->fields[0], '}') == NULL)
+        {
+
+            p = malloc(sizeof(Kilit));
+
+            if (strchr(is->fields[0], ':') != NULL)
+            {
+                remove_str_character(is->fields[0], ':');
+            }
+            else
+            {
+                printf("KILIT DOSYASI HATALI!\n");
+                printf("Hatalı Satır Numarası: %d, \nHatalı Satır: %sHata: ':' karakteri yok!\n", is->line, is->text1);
+                exit(1);
+            }
+
+            if (strchr(is->fields[0], '"') != NULL)
+                remove_str_character(is->fields[0], '"');
+
+            if (strchr(is->fields[1], '"') != NULL)
+                remove_str_character(is->fields[1], '"');
+
+            if (strchr(is->fields[1], ',') != NULL)
+                remove_str_character(is->fields[1], ',');
+
+            p->key = (char *)malloc(sizeof(char) * (strlen(is->fields[0]) + 1));
+            strcpy(p->key, is->fields[0]);
+
+            p->val = (char *)malloc(sizeof(char) * (strlen(is->fields[1]) + 1));
+            strcpy(p->val, is->fields[1]);
+
+            (void)jrb_insert_str(kilit_encrypt, p->key, new_jval_v((void *)p));
+            (void)jrb_insert_str(kilit_decrypt, p->val, new_jval_v((void *)p));
+        }
+    }
+}
