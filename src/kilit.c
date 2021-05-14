4e45b0d0768c67b73dd8ff7114d0189169c87f2b
Emre Bodur <35879677+boduremre@users.noreply.github.com>
2021-05-14 16:16:47 +0300

    Update kilit.c

--- src/kilit.c
+++ src/kilit.c
@@ -6,9 +6,9 @@
 #include "kilit.h"
 
 void print_jrb();
-void fill_jrb_from_kilit_file(JRB kilit_encrypt, JRB kilit_decrypt);
 void encrypt_file(IS is2, JRB kilitler, char *hedef_dosya_adi);
 void decrypt_file(IS is, JRB kilitlerDec, char *hedef_dosya_adi);
+
 void fill_jrb_from_kilit_file(JRB kilit_encrypt, JRB kilit_decrypt)
 {
     IS is;
