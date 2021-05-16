93e49bfe62098c16076c1dc4d7fb93e9d53b3e95
Hüseyin Yeni <63191982+G191210301@users.noreply.github.com>
2021-05-16 12:38:52 +0300

    Fonksiyonlar düzenlendi açıklamalar eklendi.

--- mrbdr.h
+++ mrbdr.h
@@ -3,10 +3,21 @@
    BSM308 Proje / Tasarım Ödevi
    Mayıs, 2021
 */
-
 #ifndef _MRBDR_H_
 #define _MRBDR_H_
 
-void remove_str_character(char *s, char delimeter);
-int getNumPipe(const char *cmd, char match);
+/* @name  : remove_str_character
+ * @desc  : Parametre olarak verilen metin değişkenin yine parametre olarak verilen silinecek_karakter değişkenini siler.
+ * @params: char *metin, char ayrac_karakter
+ * @return: void
+ */
+void metinden_karakter_sil(char *metin, char silinecek_karakter);
+
+/* @name  : tirnak_isareti_say
+ * @desc  : Parametre olarak verilen metin içerisinde " karakterini sayar.
+ * @params: const char *metin, char karakter
+ * @url   : https://stackoverflow.com/questions/23170096/how-to-count-single-or-double-quotes
+ * @return: void
+ */
+int tirnak_isareti_say(const char *metin, char karakter);
 #endif
